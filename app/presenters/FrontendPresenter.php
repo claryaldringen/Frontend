153bb541c4b790763736359958d63a9cb3726505
clary <zadram1@gmail.com>
2017-01-23 06:29:26 +0100

    Upravy pro telefon

--- app/presenters/FrontendPresenter.php
+++ app/presenters/FrontendPresenter.php
@@ -4,28 +4,57 @@ use Nette\Application\UI;
 
 class FrontendPresenter extends cms\FrontendPresenter {
 
+	protected function baseRender($url)
+	{
+		$item = parent::baseRender($url);
+		$this->template->languageId = $this->languageId;
+		return $item;
+	}
+
 	public function createComponentEmailFormCr() {
+		$label = array(36 => 'Váš e-mail:', 40 => 'Your e-mail:');
+		$error = array(
+			36 => 'Vaše e-mailová adresa není správně zadaná. Zkontrolujte, zdali jste ji zadali ve správném tvaru.',
+			40 => 'Your e-mail address is not valid.'
+		);
 		$form = new UI\Form($this, 'emailFormCr');
 		$form->addText('url')->addRule(~UI\Form::FILLED)->setAttribute('class', 'as');
-		$form->addText('email', 'Váš e-mail:')
+		$form->addText('email', $label[$this->languageId])
 			->addCondition(UI\Form::FILLED)
-			->addRule(UI\Form::EMAIL, 'Váše e-mailová adresa není správně zadaná. Zkontrolujte, zdali jste ji zadali ve správném tvaru.');
-		$form->addTextArea('message', 'Vaše zpráva:')->setRequired('Vložte prosím text vaší zprávy.');
+			->addRule(UI\Form::EMAIL, $error[$this->languageId]);
+		$label = array(36 => 'Vaše zpráva:', 40 => 'Your message:');
+		$error = array(
+			36 => 'Vložte prosím text vaší zprávy.',
+			40 => 'Please, fill your message.'
+		);
+		$form->addTextArea('message', $label[$this->languageId])->setRequired($error[$this->languageId]);
 		$form->addHidden('type','cr');
-		$form->addSubmit('send', 'Odeslat');
+		$label = array(36 => 'Odeslat', 40 => 'Send');
+		$form->addSubmit('send', $label[$this->languageId]);
 		$form->onSuccess[] = array($this, 'emailFormSubmitted');
 		return $form;
 	}
 
 	public function createComponentEmailFormSk() {
+		$label = array(36 => 'Váš e-mail:', 40 => 'Your e-mail:');
+		$error = array(
+			36 => 'Vaše e-mailová adresa není správně zadaná. Zkontrolujte, zdali jste ji zadali ve správném tvaru.',
+			40 => 'Your e-mail address is not valid.'
+		);
 		$form = new UI\Form($this, 'emailFormSk');
 		$form->addText('url')->addRule(~UI\Form::FILLED)->setAttribute('class', 'as');
-		$form->addText('email', 'Váš e-mail:')
+		$form->addText('email', $label[$this->languageId])
 			->addCondition(UI\Form::FILLED)
-			->addRule(UI\Form::EMAIL, 'Váše e-mailová adresa není správně zadaná. Zkontrolujte, zdali jste ji zadali ve správném tvaru.');
-		$form->addTextArea('message', 'Vaše zpráva:')->setRequired('Vložte prosím text vaší zprávy.');
+			->addRule(UI\Form::EMAIL, $error[$this->languageId]);
+		$label = array(36 => 'Vaše zpráva:', 40 => 'Your message:');
+		$error = array(
+			36 => 'Vložte prosím text vaší zprávy.',
+			40 => 'Please, fill your message.'
+		);
+		$form->addTextArea('message', $label[$this->languageId])->setRequired($error[$this->languageId]);
 		$form->addHidden('type','sk');
-		$form->addSubmit('send', 'Odeslat');
+		$label = array(36 => 'Odeslat', 40 => 'Send');
+		$form->addSubmit('send', $label[$this->languageId]);
 		$form->onSuccess[] = array($this, 'emailFormSubmitted');
 		return $form;
 	}
@@ -56,11 +85,11 @@ class FrontendPresenter extends cms\FrontendPresenter {
 	}
 
 	private function getImages() {
-		$folders = $this->context->getService('galleryModel')->setLanguage($this->languageId)->getFolders(31);
+		$folders = $this->context->getService('galleryModel')->setLanguage(36)->getFolders(31);
 		$this->template->images = array();
 		if(isset($folders[0])) {
 			foreach ($folders[0]['folders'] as $folder) {
-				if ($folder['id'] == 100) {
+				if ($folder['id'] == 100 && !empty($folder['images'])) {
 					foreach ($folder['images'] as $image) {
 						$this->template->images[] = $image['file'];
 					}
@@ -68,6 +97,25 @@ class FrontendPresenter extends cms\FrontendPresenter {
 				}
 			}
 		}
+	}
+
+	public function handleChangeLanguage() {
+		if($this->languageId == 40) {
+			$this->languageId = 36;
+		} else {
+			$this->languageId = 40;
+		}
+		$this->getSession('cms')->languageId = $this->languageId;
+		$this->redirect('default');
+	}
+
+	protected function renderGallery($item, $url)
+	{
+		$oldLanguageId = $this->languageId;
+		$this->languageId = 36;
+		$out = parent::renderGallery($item, $url);
+		$this->languageId = $oldLanguageId;
+		return $out;
 	}
 
 }
