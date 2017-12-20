270989e477e517b256a15afc95c988b60aa89d07
Martin Zadrazil <clary@Martins-MacBook-Pro.local>
2017-12-20 13:35:39 +0100

    Cli Presenter

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -7,6 +7,11 @@ use Nette\Application\Routers;
  */
 class RouterFactory
 {
+    private $container;
+
+    public function __construct(Nette\DI\Container $container) {
+        $this->container = $container;
+    }
 
 	/**
 	 * @return Nette\Application\IRouter
@@ -14,36 +19,40 @@ class RouterFactory
 	public function createRouter()
 	{
 		$router = new Routers\RouteList();
+        if ($this->container->parameters['consoleMode']) {
+            $router[] = new Routers\CliRouter(['action' => 'Cli:default']);
+        } else {
 
-		$router[] = new Routers\Route('/sitemap.xml',array(
-			'presenter' => 'Frontend',
-			'action' => 'sitemap',
-		));
+            $router[] = new Routers\Route('/sitemap.xml', array(
+                'presenter' => 'Frontend',
+                'action' => 'sitemap',
+            ));
 
-		$router[] = new Routers\Route('/ajax/',array(
-			'presenter' => 'Ajax',
-			'action' => 'default',
-		));
+            $router[] = new Routers\Route('/ajax/', array(
+                'presenter' => 'Ajax',
+                'action' => 'default',
+            ));
 
-		$router[] = new Routers\Route('/download/<hash>',array(
-			'presenter' => 'Frontend',
-			'action' => 'download',
-		));
+            $router[] = new Routers\Route('/download/<hash>', array(
+                'presenter' => 'Frontend',
+                'action' => 'download',
+            ));
 
-		$router[] = new Routers\Route('/admin/',array(
-			'presenter' => 'Frontend',
-			'action' => 'admin',
-		));
+            $router[] = new Routers\Route('/admin/', array(
+                'presenter' => 'Frontend',
+                'action' => 'admin',
+            ));
 
-		$router[] = new Routers\Route('/<url .+>.html',array(
-				'presenter' => 'Frontend',
-				'action' => 'default'
-		));
+            $router[] = new Routers\Route('/<url .+>.html', array(
+                'presenter' => 'Frontend',
+                'action' => 'default'
+            ));
 
-		$router[] = new Routers\Route('',array(
-				'presenter' => 'Frontend',
-				'action' => 'default'
-		));
+            $router[] = new Routers\Route('', array(
+                'presenter' => 'Frontend',
+                'action' => 'default'
+            ));
+        }
 
 		return $router;
 	}
