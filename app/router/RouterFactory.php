5798f86dabf0e50cda3adfcffdd110405ca8d6ce
Martin Zadražil <martin.zadrazil@livescore.com>
2020-08-15 11:05:38 +0200

    Secured routes

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -18,32 +18,32 @@ class RouterFactory
 		$router[] = new Routers\Route('/sitemap.xml',array(
 			'presenter' => 'Frontend',
 			'action' => 'sitemap',
-		));
+		), Routers\Route::SECURED);
 
 		$router[] = new Routers\Route('/ajax/',array(
 			'presenter' => 'Ajax',
 			'action' => 'default',
-		));
+		), Routers\Route::SECURED);
 
 		$router[] = new Routers\Route('/download/<hash>',array(
 			'presenter' => 'Frontend',
 			'action' => 'download',
-		));
+		), Routers\Route::SECURED);
 
 		$router[] = new Routers\Route('/admin/',array(
 			'presenter' => 'Frontend',
 			'action' => 'admin',
-		));
+		), Routers\Route::SECURED);
 
 		$router[] = new Routers\Route('/<url .+>.html',array(
 				'presenter' => 'Frontend',
 				'action' => 'default'
-		));
+		), Routers\Route::SECURED);
 
 		$router[] = new Routers\Route('',array(
 				'presenter' => 'Frontend',
 				'action' => 'default'
-		));
+		), Routers\Route::SECURED);
 
 		return $router;
 	}
